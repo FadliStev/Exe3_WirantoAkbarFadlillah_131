f079606f86dcf9155d2f366c6c9a2c7bc3f65f11
FadliStev <wiranto.akbar.ft21@mail.umy.ac.id>
2022-11-23 15:38:03 +0700

    add class and method

--- Exe3_A/Program.cs
+++ Exe3_A/Program.cs
@@ -8,9 +8,32 @@ namespace Exe3_A
 {
     class node
     {
+        /*creates nodes for the circular nexted list*/
         public int rollnumber;
         public string name;
         public node next;
+    }
+    class Circularlist
+    {
+        node LAST;
+
+        public Circularlist()
+        {
+            LAST = null;
+        }
+        public bool Search(int rollNo, ref node previous, ref node current)
+            /*Searches for the specified node*/
+        {
+            for(previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollnumber)
+                    return (true);/*returns true if teh node is found*/
+            }
+            if (rollNo == LAST.rollnumber)/*if the node is present at the end*/
+                return true;
+            else
+                return (false);/*returns false if the node is not found*/
+        }
     }
 
     internal class Program
